d1d554711226b08c325ea9bc704f6ac0cf1913cc
joyscript <joyscript7@gmail.com>
2022-02-05 00:20:13 +0300

    feat: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,29 @@ const isDev = 'development';
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
   devtool: isDev ? 'source-map' : 'none',
-  // watch: isDev,
 
   entry: ['./src/index.js', './src/sass/style.scss'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    // assetModuleFilename: 'images/[hash][ext][query]',
   },
 
   module: {
     rules: [
-      {
-        test: /\.js$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
+      // {
+      //   test: /\.js$/i,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env'],
+      //     },
+      //   },
+      // },
       {
         test: /\.scss$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
@@ -37,18 +34,11 @@ module.exports = {
         test: /\.(png|jpg|svg)$/i,
         type: 'asset/resource',
       },
-      // {
-      //   test: /\.html$/,
-      //   loader: 'html-loader',
-      // },
     ],
   },
 
   plugins: [
     new CleanWebpackPlugin(),
-    // new HtmlWebpackPlugin({
-    //   template: 'index.html',
-    // }),
     new MiniCssExtractPlugin({
       filename: 'style.css',
     }),
